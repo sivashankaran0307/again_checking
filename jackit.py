be3b5b0a6b5e0b73f3bbb8d07cdfc018241ed46f
Sivashankaran <sivashankaran0307@gmail.com>
2020-12-17 01:07:58 +0530

    A samuel jackson styled print statement

--- jackit.py
+++ jackit.py
@@ -1,3 +1,7 @@
 #! /usr/bin/env python3
 
-print("jackpot")
+a=int(input("Please enter a number below 10 - "))
+if 0<a<10:
+	print("jackpot")
+else:
+	print("I asked you to do one thing, but you Mr.Big shot with a big attitude having a tiny peepee refuse to obey me")
